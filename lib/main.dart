ceb26f71d6af72e027116a248c61329b8e8a0de7
dev.khaled <khaled_salehalali@hotmail.com>
2020-10-09 14:40:56 +0300

    new2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//m
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sooq1alzour/Service/PushNotificationService.dart';
@@ -22,31 +23,29 @@ import 'Auth/NewLogin.dart';
 void setupLocator() {
   GetIt.I.registerLazySingleton(() => PushNotificationService());
 }
+
 void main() {
   setupLocator();
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //initialRoute: Home.id,
-      routes: {
-        SearchUi.id : (context) => SearchUi(),
-        Ads.id : (context) => Ads(),
-        Home.id : (context) => Home(),
-        MyAccount.id : (context) => MyAccount(),
-        AddNewAd.id : (context) => AddNewAd(),
-        Food.id : (context) => Food(),
-        Clothes.id : (context) => Clothes(),
-        Games.id : (context) => Games(),
-        Farming1.id : (context) => Farming1(),
-        Livestock.id : (context) => Livestock(),
-        Homes.id : (context) => Homes(),
-        OccupationsAndServices.id : (context) => OccupationsAndServices(),
-        Mobile.id : (context) => Mobile(),
-        DevicesAndElectronics.id : (context) => DevicesAndElectronics(),
-        CarsAndMotorCycles.id : (context) => CarsAndMotorCycles(),
-      },
-      home: NewLogin(),
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    //initialRoute: Home.id,
+    routes: {
+      SearchUi.id: (context) => SearchUi(),
+      Ads.id: (context) => Ads(),
+      Home.id: (context) => Home(),
+      MyAccount.id: (context) => MyAccount(),
+      AddNewAd.id: (context) => AddNewAd(),
+      Food.id: (context) => Food(),
+      Clothes.id: (context) => Clothes(),
+      Games.id: (context) => Games(),
+      Farming1.id: (context) => Farming1(),
+      Livestock.id: (context) => Livestock(),
+      Homes.id: (context) => Homes(),
+      OccupationsAndServices.id: (context) => OccupationsAndServices(),
+      Mobile.id: (context) => Mobile(),
+      DevicesAndElectronics.id: (context) => DevicesAndElectronics(),
+      CarsAndMotorCycles.id: (context) => CarsAndMotorCycles(),
+    },
+    home: NewLogin(),
+  ));
 }
-
